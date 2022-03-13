490bccbbc703ca23ad5c7b62296348dae3edaee7
zackees <z@zackees.com>
2022-03-13 16:08:46 -0700

    fix

--- run.py
+++ run.py
@@ -22,9 +22,17 @@ def main():
         f'python fileserver.py --port {args.port}', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=here, shell=True)
 
     try:
-        ngrok_tunnel = ngrok.connect(3306, "http")
-        print(f"  LOCAL:  http://127.0.0.1:{args.port}")
-        print(f"\nFILESERVER:\n  PUBLIC: {ngrok_tunnel.public_url}")
+        public_url = "Not available"
+        try:
+            ngrok_tunnel = ngrok.connect(args.port, "http")
+            public_url = ngrok_tunnel.public_url
+        except Exception as e:
+            print(f"{__file__}: Couldn't open up ngrok tunnel because {e}")
+        print(
+            "UPLOAD SERVER:\n"
+            f"  LOCAL: http://127.0.0.1:{args.port}\n"
+            f"  PUBLIC: {public_url}"
+        )
         while True:
             time.sleep(.1)
     except KeyboardInterrupt:
